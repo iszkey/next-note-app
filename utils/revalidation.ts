b524bf64ce4d825d33450a91b29c4d5b9d8d5f9c
iszkey <kisozaki1026@gmail.com>
2023-02-19 10:11:31 +0900

    fetchするURLの誤りを修正

--- utils/revalidation.ts
+++ utils/revalidation.ts
@@ -1,7 +1,8 @@
 export const revalidateList = () => {
-    fetch('api/revalidate');
+    fetch('/api/revalidate');
 };
 
 export const revalidateSingle = (id: string) => {
-    fetch(`api/revalidate/${id}`);
+    fetch(`/api/revalidate/${id}`);
+    // fetch(`api/revalidate/${id}`); の場合はエラーになるので注意
 };
